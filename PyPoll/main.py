c2633b5dd9f26b4a56cd16aeb549b3d88b18a70b
Stuart Yates <sjwyates@Stuarts-MacBook-Pro.local>
2020-10-04 12:19:03 -0600

    PyPoll prints analysis to terminal

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,9 +1,33 @@
 import os
 import csv
 
-csv_path = os.path.join('Resources', 'election_data.csv')
+tally_dict = {}
+total_votes = 0
 
-with open(csv_path, 'r') as csv_file:
+input_path = os.path.join('Resources', 'election_data.csv')
+
+with open(input_path, 'r') as csv_file:
     csv_reader = csv.reader(csv_file)
 
-    
+    next(csv_reader)
+
+    for row in csv_reader:
+        if row[2] not in tally_dict:
+            tally_dict[row[2]] = 1
+        else:
+            tally_dict[row[2]] += 1
+        total_votes += 1
+
+winner = max(tally_dict, key=tally_dict.get)
+
+def get_pct(candidate, votes):
+    vote_pct = round(100 * votes / total_votes, 3)
+    return f"{candidate}: {vote_pct}% ({votes})"
+
+result_list = [get_pct(candidate, votes) for candidate, votes in tally_dict.items()]
+
+bar = "-------------------------"
+analysis = ["Election Results", bar, f"Total Votes: {total_votes}", bar] + result_list + [bar, f"Winner: {winner}", bar]
+
+for line in analysis:
+    print(line)
