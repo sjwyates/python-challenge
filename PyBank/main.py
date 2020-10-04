b66e907b80f70937f620eb7b6c6534742fbfce4b
Stuart Yates <sjwyates@Stuarts-MacBook-Pro.local>
2020-10-04 10:06:35 -0600

    Print to csv

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,30 +2,43 @@ import os
 import csv
 import operator as op
 
+column_names = []
 budget_list = []
-total = 0
+total_pl = 0
 
-csv_path = os.path.join('Resources', 'budget_data.csv')
+input_path = os.path.join('Resources', 'budget_data.csv')
+output_path = os.path.join('Output', 'budget_results.csv')
 
-with open(csv_path, 'r') as csv_file:
+with open(input_path, 'r') as csv_file:
     csv_reader = csv.reader(csv_file)
 
     column_names = next(csv_reader)
 
     for row in csv_reader:
         budget_list.append((row[0], int(row[1])))
-        total += int(row[1])
+        total_pl += int(row[1])
 
 budget_list.sort(key = lambda x: x[1])
 
 num_months = len(budget_list)
-avg_change = total / num_months
+avg_change = total_pl / num_months
 greatest_inc = budget_list[-1]
 greatest_dec = budget_list[1]
 
-print("Financial Analysis")
-print("----------------------------")
-print(f"Total Months: {num_months}")
-print(f"Average Change: {avg_change}")
-print(f"Greatest Increase in Profits: {greatest_inc[0]} ({greatest_inc[1]})")
-print(f"Greatest Decrease in Profits: {greatest_dec[0]} ({greatest_dec[1]})")
+financial_analysis = ["Financial Analysis", "----------------------------", 
+f"Total Months: {num_months}", f"Total: ${total_pl}", f"Average Change: {avg_change}", 
+f"Greatest Increase in Profits: {greatest_inc[0]} ({greatest_inc[1]})", 
+f"Greatest Decrease in Profits: {greatest_dec[0]} ({greatest_dec[1]})"]
+
+print(*financial_analysis, sep = "\n")
+
+with open(output_path, 'w') as results_file:
+    csv_writer = csv.writer(results_file)
+
+    csv_writer.writerows(financial_analysis)
+    csv_writer.writerow("")
+    csv_writer.writerow("Data")
+    csv_writer.writerow("----------------------------")
+    csv_writer.writerow("")
+    csv_writer.writerow(column_names)
+    csv_writer.writerows(budget_list)
