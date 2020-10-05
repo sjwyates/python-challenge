dc6a058c1bee7b000ad9eef80de9eb76effbee53
Stuart Yates <sjwyates@Stuarts-MacBook-Pro.local>
2020-10-05 17:35:35 -0600

    Commented code

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,43 +1,62 @@
 import os
 import csv
-import operator as op
 
+# List of tuples (month, profits/losses) to hold slightly cleaned data
 budget_list = []
+# Incrementing tally for total profits/losses
 total_pl = 0
+# List of month-to-month change in profits/losses
 pl_changes = []
 
+# File paths
 input_path = os.path.join('Resources', 'budget_data.csv')
 output_path = os.path.join('Output', 'budget_results.txt')
 
+# Open CSV raw data file and instantiate the CSV reader
 with open(input_path, 'r') as csv_file:
     csv_reader = csv.reader(csv_file)
 
+    # Skip header row, then get data from first row
     next(csv_reader)
     first_row = next(csv_reader)
     prev_pl = int(first_row[1])
     budget_list.append((first_row[0], prev_pl))
 
+    # For each row after the first...
     for row in csv_reader:
+        # Get date and profits/losses, coerced to integer
         date = row[0]
         pl = int(row[1])
+        # Append a tuple with the date and profits/losses
         budget_list.append((date, pl))
+        # Increment total profits/losses by current value
         total_pl += pl
+        # Subtract previous value and append to changes list
         pl_changes.append(pl - prev_pl)
+        # Reset previous previous profits/losses to current value
         prev_pl = pl
         
+# Sort list by the 1nth item in each tuple (profits/losses)
 budget_list.sort(key = lambda x: x[1])
 
+# Get number of months
 num_months = len(budget_list)
+# Sum the changes list and divide by num_months to get the average
 avg_change = sum(pl_changes) / (num_months - 1)
+# Greatest increase is last item in sorted list; greatest decrease is the first
 greatest_inc = budget_list[-1]
 greatest_dec = budget_list[0]
 
+# Create a list of strings corresponding to each line of the output
 analysis = ["Financial Analysis", "----------------------------", 
 f"Total Months: {num_months}", f"Total: ${total_pl}", f"Average Change: ${round(avg_change, 2)}", 
 f"Greatest Increase in Profits: {greatest_inc[0]} (${greatest_inc[1]})", 
 f"Greatest Decrease in Profits: {greatest_dec[0]} (${greatest_dec[1]})"]
 
+# Open up the new text file to write the results to
 with open(output_path, 'w') as results_file:
+    # Single loop to print to terminal and write to file
     for line in analysis:
         print(line)
+        # printf format string to add line breaks after each line
         results_file.write("%s\n" % line)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,36 +1,55 @@
 import os
 import csv
 
+# Dictionary to store tallies as name: votes
 tally_dict = {}
+# Counter for total votes
 total_votes = 0
 
+# Input and output file paths
 input_path = os.path.join('Resources', 'election_data.csv')
 output_path = os.path.join('Output', 'election_results.txt')
 
+# Open up the CSV file and instantiate the CSV reader
 with open(input_path, 'r') as csv_file:
     csv_reader = csv.reader(csv_file)
 
+    # Ignore the header row
     next(csv_reader)
 
+    # For each remaining row...
     for row in csv_reader:
+        # If that candidate isn't in the dictionary yet...
         if row[2] not in tally_dict:
+            # Add them and start their tally at 1 vote
             tally_dict[row[2]] = 1
+        # Otherwise...
         else:
+            # Increment their existing tally by 1 vote
             tally_dict[row[2]] += 1
+        # Either way, increment total votes by 1
         total_votes += 1
 
+# Find the max value in the dictionary and return its key
 winner = max(tally_dict, key=tally_dict.get)
 
+# Function to calc vote percent and return formatted string
 def get_pct(candidate, votes):
     vote_pct = round(100 * votes / total_votes, 3)
     return f"{candidate}: {vote_pct}% ({votes})"
 
+# List comprehension applies that function to each item in the dictionary
 result_list = [get_pct(candidate, votes) for candidate, votes in tally_dict.items()]
 
+# Because the same bar is used 4 times
 bar = "-------------------------"
+# Make a list of strings, each representing 1 line in the analysis,
+# by combining 3 lists (which is super simple in Python)
 analysis = ["Election Results", bar, f"Total Votes: {total_votes}", bar] + result_list + [bar, f"Winner: {winner}", bar]
 
 with open(output_path, 'w') as results_file:
+    # Single loop to print to terminal and write to file
     for line in analysis:
         print(line)
+        # printf format string to add line breaks after each line
         results_file.write("%s\n" % line)
